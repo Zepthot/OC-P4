1be6f24a7cdfdf780710645af3732bea7f2cb96f
Zepthot <ypoulard.theuil@gmail.com>
2024-05-23 23:09:31 +0200

    [func] close modal function + beside; [fix] update css of close before after;

--- starterOnly/dist/modal.js
+++ starterOnly/dist/modal.js
@@ -10,10 +10,26 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const btnClose = document.querySelector(".close");
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-function launchModal() {
+function launchModal(event) {
     modalbg
         ? (modalbg.style.display = "block")
         : console.error("Element with class 'bground' not found.");
+    event.stopPropagation();
 }
+btnClose === null || btnClose === void 0 ? void 0 : btnClose.addEventListener("click", closeModal);
+function closeModal() {
+    modalbg
+        ? (modalbg.style.display = "none")
+        : console.error("Element with class 'bground' not found.");
+}
+window.onclick = (event) => {
+    const target = event.target;
+    if (modalbg &&
+        modalbg.style.display === "block" &&
+        !target.closest(".content")) {
+        modalbg.style.display = "none";
+    }
+};
 //# sourceMappingURL=modal.js.map
--- starterOnly/src/modal.ts
+++ starterOnly/src/modal.ts
@@ -12,12 +12,38 @@ const modalbg = document.querySelector<HTMLElement>(".bground");
 const modalBtn = document.querySelectorAll<HTMLElement>(".modal-btn");
 const formData = document.querySelectorAll<HTMLElement>(".formData");
 
+const btnClose = document.querySelector<HTMLElement>(".close");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
-function launchModal() {
+function launchModal(event: Event) {
   modalbg
     ? (modalbg.style.display = "block")
     : console.error("Element with class 'bground' not found.");
+
+  event.stopPropagation();
 }
+
+// close modal event
+btnClose?.addEventListener("click", closeModal);
+
+// close modal button
+function closeModal() {
+  modalbg
+    ? (modalbg.style.display = "none")
+    : console.error("Element with class 'bground' not found.");
+}
+
+// close modal beside
+window.onclick = (event: MouseEvent) => {
+  const target = event.target as HTMLElement;
+  if (
+    modalbg &&
+    modalbg.style.display === "block" &&
+    !target.closest(".content")
+  ) {
+    modalbg.style.display = "none";
+  }
+};
