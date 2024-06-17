9273f3191965477c2cebcf17e188c01cf67b8ace
Zepthot <127233824+Zepthot@users.noreply.github.com>
2024-06-17 17:56:07 +0200

    Delete starterOnly/src directory

--- starterOnly/src/nav.ts
+++
@@ -1,8 +0,0 @@
-function editNav() {
-  let x = document.getElementById("myTopnav");
-  x
-    ? x.className === "topnav"
-      ? (x.className += " responsive")
-      : (x.className = "topnav")
-    : console.error("Element with ID 'myTopnav' not found.");
-}
